4f4bdd86fa9033e18d41b27c0d150625af8fa87a
Alasdair Monk <alasdair.monk@gmail.com>
2014-01-30 18:43:37 +0000

    Latest

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var makeid = require('./makeid');
 var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 
 var app = express();
@@ -17,12 +18,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 
+base_url = "http://almonk.com"
+
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
+    base_url = "http://0.0.0.0:3000/"
 }
 
-base_url = "http://almonk.com/"
 
 app.get('/shorten', function(req, res){
     shortened_id = makeid();
@@ -50,16 +53,20 @@ app.get('/:id', function(req, res){
     });
 });
 
-function makeid()
-{
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+app.put('/edit/:id', function(req, res){
+    console.log("Lookup " + req.param("url"));
 
-    for( var i=0; i < 5; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    var new_url = req.param("url");
 
-    return text;
-}
+    redis.set(req.params.id, req.param("url"));
+
+    res.send(
+        {
+            shortened_url: base_url + req.params.id
+        }
+    );
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Lynn's a good worker");
